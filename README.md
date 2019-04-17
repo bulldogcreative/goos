be7b9d6c9dcf9a31750ea73961c3282c65949c76
Levi Durfee <levi.durfee@gmail.com>
2019-04-17 09:06:40 -0400

    Refactor Handler Generator

--- README.md
+++ README.md
@@ -2,16 +2,41 @@
 
 _go object storage_ proxy.
 
-## Environment Variables
+## Example Usage
 
-These **must** be set.
+```go
+package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/bulldogcreative/goos/pkg/goos"
+)
+
+func main() {
+	fmt.Println("Starting")
+
+	g := &goos.Goos{
+		KeyID:    os.Getenv("aws_access_key_id"),
+		Secret:   os.Getenv("aws_secret_access_key"),
+		Endpoint: "https://nyc3.digitaloceanspaces.com",
+		Region:   "us-east-2",
+		Bucket:   os.Getenv("aws_bucket"),
+	}
+
+	// logwriter, e := syslog.New(syslog.LOG_NOTICE, "goos")
+	// if e == nil {
+	// 	log.SetOutput(logwriter)
+	// }
+
+	handler := g.Handler()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
 ```
-aws_access_key_id = <YOUR_ACCESS_KEY_ID>
-aws_secret_access_key = <YOUR_SECRET_ACCESS_KEY>
-aws_region = <COUNTRY-REGION-NUM>
-```
 
-### Response Headers
+## Response Headers
 
 [Last-Modified](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Last-Modified)
--- pkg/goos/goos.go
+++ pkg/goos/goos.go
@@ -3,66 +3,71 @@ package goos
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strconv"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// S3Handler will return the handler we need
-func S3Handler(s *session.Session, bucket string) http.HandlerFunc {
-	svc := s3.New(s)
+// Goos holds the information to connect to an S3 compatible Object Storage Service
+type Goos struct {
+	KeyID    string
+	Secret   string
+	Endpoint string
+	Region   string
+	Bucket   string
+}
+
+func (g *Goos) session() *session.Session {
+	s3Config := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(g.KeyID, g.Secret, ""),
+		Endpoint:    aws.String(g.Endpoint),
+		Region:      aws.String(g.Region),
+	}
+	sess := session.New(s3Config)
+
+	return sess
+}
+
+// Handler will return the handler we need
+func (g *Goos) Handler() http.HandlerFunc {
+	svc := s3.New(g.session())
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
 		if r.URL.String() == "/" {
-			log(r.RemoteAddr, r.URL.String(), "/")
+			logMessage(r.RemoteAddr, r.URL.String(), "/")
 			notFound(w)
 			return
 		}
 
 		url, err := url.QueryUnescape(r.URL.String())
 		if err != nil {
-			log(r.RemoteAddr, r.URL.String(), "404")
+			logMessage(r.RemoteAddr, r.URL.String(), "404")
 			notFound(w)
 			return
 		}
 
 		input := &s3.GetObjectInput{
-			Bucket: aws.String(bucket),
+			Bucket: aws.String(g.Bucket),
 			Key:    aws.String(url),
 		}
 		result, err := svc.GetObject(input)
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				case s3.ErrCodeNoSuchKey:
-					log(r.RemoteAddr, url, "404")
-					notFound(w)
-					return
-				default:
-					log(r.RemoteAddr, url, "404")
-					notFound(w)
-					return
-				}
-			} else {
-				// Print the error, cast err to awserr.Error to get the Code and
-				// Message from an error.
-				log(r.RemoteAddr, url, "404")
-				notFound(w)
-				return
-			}
-
+			logMessage(r.RemoteAddr, url, "404")
+			notFound(w)
+			return
 		}
 
 		w.Header().Set("Content-Length", strconv.FormatInt(*result.ContentLength, 10))
 		w.Header().Set("Last-Modified", result.LastModified.Format("Mon, 02 Jan 2006 15:04:05 MST"))
 		w.Header().Set("Expires", time.Now().AddDate(60, 0, 0).Format(http.TimeFormat))
-		w.Header().Set("Cache-Control", "max-age:290304000, public")
+		w.Header().Set("Cache-Control", "max-age:290304000")
 		w.Header().Set("Etag", *result.ETag)
 
 		// fmt.Println(result)
@@ -70,10 +75,10 @@ func S3Handler(s *session.Session, bucket string) http.HandlerFunc {
 		bf := new(bytes.Buffer)
 		bf.ReadFrom(result.Body)
 
-		w.Write([]byte(bf.String()))
+		w.Write(bf.Bytes())
 
 		// Print Request Details
-		log(r.RemoteAddr, url, "200")
+		logMessage(r.RemoteAddr, url, "200")
 	}
 
 	return http.HandlerFunc(fn)
@@ -85,6 +90,7 @@ func notFound(w http.ResponseWriter) {
 	w.Write([]byte("Not Found."))
 }
 
-func log(remote string, url string, status string) {
+func logMessage(remote string, url string, status string) {
 	fmt.Println("[" + time.Now().Format(time.RFC3339) + "] [" + remote + "] " + "[" + url + "] " + "[" + status + "]")
+	log.Print("[" + remote + "] [" + url + "] [" + status + "]")
 }

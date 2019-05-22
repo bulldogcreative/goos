3fc8040080cb0a50716fe4a90e21b4df32a968e1
Levi Durfee <levi.durfee@gmail.com>
2019-05-22 09:09:10 -0400

    Create a build script for linux/windows

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+version=v1.0.0
+
+env GOOS=windows GOARCH=amd64 go build -o dist/goos.exe cmd/main.go
+cd dist
+tar -zcvf windows-$version.tar.gz goos.exe
+rm goos.exe
+cd ..
+
+env GOOS=linux GOARCH=amd64 go build -o dist/goos cmd/main.go
+cd dist
+tar -zcvf linux-$version.tar.gz goos
+rm goos
+cd ..
